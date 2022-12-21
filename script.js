9fd913da1a651bad0a332a6e5d288d8fd4b719c6
LucaMarchesani <lucamarchesani.lavoro@gmail.com>
2022-12-21 17:51:28 +0100

    add v-show

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 const { createApp } = Vue;
 
 createApp({
-  data() {
-    return {
-        mailList: [],
-        randomEmail: null,
-        
-      
-    }
-  },
-  methods: {
-    getRandomEmail(){
-        let i = 0;
-        while (i < 10){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((response) => {
-                console.log(response.data);
-                this.randomEmail = response.data.response;
-                this.mailList.push(this.randomEmail);
-            })
-            i++
+    data() {
+        return {
+            mailList: [],
+            randomEmail: null,
+
+
         }
+    },
+    methods: {
+        getRandomEmail() {
+            let i = 0;
+            while (i < 10) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        console.log(response.data);
+                        this.randomEmail = response.data.response;
+                        this.mailList.push(this.randomEmail);
+                    })
+                i++
+            }
+        }
+    },
 
-    }
-    
-  },
-
-  created() {
-    this.getRandomEmail();
-  },
+    created() {
+        this.getRandomEmail();
+    },
 
 }).mount('#app');
